24ad447fcbe46d372d08c04fe018e45070f9953d
Marvin - The Flint <Marv.Murmann@gmx.net>
2019-02-12 13:45:46 +0100

    Added Gameplay loading order

--- js/switchers.js
+++ js/switchers.js
@@ -8,6 +8,9 @@ $(function(){
       if( $(this).hasClass("rune-choice")){
         $(".rune-choice").removeClass("active-switcher");
         $(this).addClass("active-switcher"); 
+        var runeID = $(this).attr("id");
+        var runeLink = "partials/runes/" + runeID + ".html";
+        $("#runePartial").load(runeLink);      
       } 
       else if( $(this).hasClass("lane-choice")){
         $(".lane-choice").removeClass("active-switcher");
@@ -18,5 +21,21 @@ $(function(){
         $(this).addClass("active-switcher"); 
       }    
     }
+    if( $("#toplane").hasClass("active-switcher") && $("#af-ap").hasClass("active-switcher") ){
+      $("#gameplay").load("partials/gameplay/gameplayAPtop.html");
+    }
+    else if( $("#toplane").hasClass("active-switcher") && $("#af-ad").hasClass("active-switcher") ){
+      $("#gameplay").load("partials/gameplay/gameplayADtop.html");
+    }
+    else if( $("#midlane").hasClass("active-switcher") && $("#af-ap").hasClass("active-switcher") ){
+      $("#gameplay").load("partials/gameplay/gameplayAPtop.html");
+    }
+    else if( $("#botlane").hasClass("active-switcher") && $("#af-ad").hasClass("active-switcher") ){
+      $("#gameplay").load("partials/gameplay/gameplayADbot.html");
+    }
+    else if( $("#botlane").hasClass("active-switcher") && $("#af-ap").hasClass("active-switcher") ){
+      $("#gameplay").load("partials/gameplay/gameplayAPbot.html");
+    }
   })
 })
+
