87b512e6152e1d06908aa1c211a68bffe514f082
Marvin - The Flint <Marv.Murmann@gmx.net>
2019-02-12 12:28:41 +0100

    organized re-structure, cleaned up js

--- js/experiments.js
+++ js/experiments.js
@@ -19,4 +19,21 @@ function toggleSection(div_id){
      document.getElementById(div_id).classList.add('toggeled');
    }
 }
+
+function setRune(symbol_id, div_id){
+          var divs = ['#display-klepto', '#display-phaserush', '#display-electrocute', '#display-pta', '#display-conqueror', '#display-fleet'];
+          var symbols = ['#klepto', '#phaserush', '#electrocute', '#pta', '#conqueror', '#fleet'];
+          for (var i = 0; i < divs.length; i++) {
+             //document.getElementById(divs[i]).classList.remove('active');
+             $(divs[i]).removeClass("active");
+             //document.getElementById(symbols[i]).classList.remove('active-switcher');
+             $(symbols[i]).removeClass("active-switcher");
+
+          };
+          //document.getElementById(div_id).classList.add('active');
+          $(div_id).addClass("active");
+          //document.getElementById(symbol_id).classList.add('active-switcher');
+          $(symbol_id).addClass("active-switcher");        
+
+}
 */
--- js/switchers.js
+++ js/switchers.js
@@ -1,34 +1,22 @@
 
-function setActive(symbol_id, div_id){
-          var divs = ['#display-klepto', '#display-phaserush', '#display-electrocute', '#display-pta', '#display-conqueror', '#display-fleet'];
-          var symbols = ['#klepto', '#phaserush', '#electrocute', '#pta', '#conqueror', '#fleet'];
-          for (var i = 0; i < divs.length; i++) {
-             //document.getElementById(divs[i]).classList.remove('active');
-             $(divs[i]).removeClass("active");
-             //document.getElementById(symbols[i]).classList.remove('active-switcher');
-             $(symbols[i]).removeClass("active-switcher");
-
-          };
-          //document.getElementById(div_id).classList.add('active');
-          $(div_id).addClass("active");
-          //document.getElementById(symbol_id).classList.add('active-switcher');
-          $(symbol_id).addClass("active-switcher");
-
-          if ($("#display-klepto").hasClass("active") || $("#display-phaserush").hasClass("active") || $("#display-electrocute").hasClass("active")){
-            $("#buildPartial").load("partials/buildsAP.html");
-            $("#gameplay").load("partials/gameplayAP.html");
-            $("#tricks").load("partials/tricksAP.html");
-            $("#matchups").load("partials/matchups.html");
-              }
-          else if( $("#display-pta").hasClass("active") || $("#display-conqueror").hasClass("active") || $("#display-fleet").hasClass("active")){
-            $("#buildPartial").load("partials/buildsAD.html");
-            $("#gameplay").load("partials/gameplayAD.html");
-            $("#tricks").load("partials/tricksAD.html");
-            $("#matchups").load("partials/matchups.html");
-            }
-
-}
-
-$(".onclick").click(function(){
-  $(".title").toggle();
+$(function(){
+  $(".switcher").click(function(){    
+    if( $(this).hasClass("active-switcher")){
+      $(this).removeClass("active-switcher");
+    }
+    else{
+      if( $(this).hasClass("rune-choice")){
+        $(".rune-choice").removeClass("active-switcher");
+        $(this).addClass("active-switcher"); 
+      } 
+      else if( $(this).hasClass("lane-choice")){
+        $(".lane-choice").removeClass("active-switcher");
+        $(this).addClass("active-switcher"); 
+      }
+      else if( $(this).hasClass("af-choice")){
+        $(".af-choice").removeClass("active-switcher");
+        $(this).addClass("active-switcher"); 
+      }    
+    }
+  })
 })
