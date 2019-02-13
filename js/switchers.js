14b5748c6eb52a7ac8b79385853b3fc8e9219eea
Marvin - The Flint <Marv.Murmann@gmx.net>
2019-02-13 18:11:38 +0100

    added ability max

--- js/switchers.js
+++ js/switchers.js
@@ -19,29 +19,40 @@ $(function(){
       else if( $(this).hasClass("af-choice")){
         $(".af-choice").removeClass("active-switcher");
         $(this).addClass("active-switcher"); 
-      }    
+      }
+      else if( $(this).hasClass("ability-choice")){
+        $(".ability-choice").removeClass("active-switcher");
+        $(this).addClass("active-switcher"); 
+      }   
     }
     if( $("#toplane").hasClass("active-switcher") && $("#af-ap").hasClass("active-switcher") ){
       $("#gameplay").load("partials/gameplay/gameplayAPtop.html");
       $("#builds").load("partials/builds/buildsAP.html");
+      $("#tricks").load("partials/tricks/tricksAP.html");
     }
     else if( $("#toplane").hasClass("active-switcher") && $("#af-ad").hasClass("active-switcher") ){
       $("#gameplay").load("partials/gameplay/gameplayADtop.html");
       $("#builds").load("partials/builds/buildsADtop.html");
+      $("#tricks").load("partials/tricks/tricksAD.html");
     }
     else if( $("#midlane").hasClass("active-switcher") && $("#af-ap").hasClass("active-switcher") ){
       $("#gameplay").load("partials/gameplay/gameplayAPtop.html");
       $("#builds").load("partials/builds/buildsAP.html");
+      $("#tricks").load("partials/tricks/tricksAP.html");
     }
     else if( $("#botlane").hasClass("active-switcher") && $("#af-ad").hasClass("active-switcher") ){
       $("#gameplay").load("partials/gameplay/gameplayADbot.html");
       $("#builds").load("partials/builds/buildsADbot.html");
+      $("#tricks").load("partials/tricks/tricksAD.html");
     }
     else if( $("#botlane").hasClass("active-switcher") && $("#af-ap").hasClass("active-switcher") ){
       $("#gameplay").load("partials/gameplay/gameplayAPbot.html");
       $("#builds").load("partials/builds/buildsAP.html");
+      $("#tricks").load("partials/tricks/tricksAP.html");
     }
     $(".welcome").css("display", "none");
+    $("#abilities").css("display", "flex");
+    
   })
 })
 
